1dbdf7567fc364eb74e0146533f8189c5a6c7eaa
Buddika Abeykoon <buddikab97@outlook.com>
2023-09-12 14:11:43 +0530

    Initial commit

---
+++ src/TestComplexity.java
@@ -0,0 +1,24 @@
+public class TestComplexity {
+
+    public static void main(String[] args) {
+        double now = System.currentTimeMillis();
+
+        TestComplexity demo = new TestComplexity();
+        System.out.println(demo.findSum(99999)) ;
+
+        System.out.println("Time taken - " + (System.currentTimeMillis() - now) + " millisecs.");
+    }
+
+    public int findSum(int n) {
+        return n * (n + 1) / 2;
+    }
+
+// 	public int findSum(int n) {
+// 		int sum = 0;
+
+// 		for(int i= 1; i <= n; i++) {
+// 			sum = sum + i;
+// 		}
+// 		return sum;
+// 	}
+}
